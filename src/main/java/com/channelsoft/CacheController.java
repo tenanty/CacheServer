6c0c477b862606cdad7412db756fad98f682c8d3
yuanshun <yuanshun@channelsoft.com>
2017-11-25 08:45:22 +0800

    名字更换

--- src/main/java/com/channelsoft/CacheController.java
+++ src/main/java/com/channelsoft/CacheController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,26 +14,28 @@ import java.util.UUID;
  * Created by yuanshun on 2017/11/19.
  */
 @RestController
-public class HelloController {
+public class CacheController {
 
-    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+    private Logger logger = LoggerFactory.getLogger(CacheController.class);
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @RequestMapping("/setKey")
-    public String setKey(@RequestParam(name = "message",required = false)String message){
-        logger.debug("进入 HelloController.setKey() message:{} .",message);
+    public String setKey(@RequestParam(name = "param",required = false)String param){
+        logger.debug("CacheController.setKey() param:{} start.",param);
         String uuid = new String();
         uuid = UUID.randomUUID().toString();
-        stringRedisTemplate.opsForValue().set(uuid, "Springboot redis:"+uuid);
+        stringRedisTemplate.opsForValue().set(uuid, param);
+        logger.debug("CacheController.setKey() key:{} param:{} ok.",uuid,param);
         return stringRedisTemplate.opsForValue().get(uuid);
     }
 
     @RequestMapping("/getKey")
     public String getKey(@RequestParam(name = "key" ,required = false )String key){
-        logger.debug("进入 HelloController.getKey() key:{}.",key);
+        logger.debug("CacheController.getKey() key:{} start.",key);
         String reuslt = stringRedisTemplate.opsForValue().get(key);
+        logger.debug("CacheController.getKey() key:{} param:{} start.",key,reuslt);
         return reuslt;
     }
 }
